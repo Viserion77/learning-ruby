2cf609fea6143fbac24a186124d1fa6f3f6dc8dc
Viserion77 <jeferson.a.oficial@gmail.com>
2022-07-19 15:30:51 -0300

    aula 4

---
+++ [youtube] Linguagem Ruby/aula_4/aula.rb
@@ -0,0 +1,23 @@
+
+a = 1
+if a == 1
+  puts "a é igual a 1"
+elsif a == 2
+  puts "a é igual a 2"
+else
+  puts "a não é igual a 1 nem a 2"
+end
+if a == 1 then puts "a é igual a 1"
+
+case a
+when 1
+  puts "a é igual a 1"
+when 2
+  puts "a é igual a 2"
+else
+  puts "a não é igual a 1 nem a 2"
+end
+
+a = false == true ? 1 : 0
+puts "a é diferente de 1" unless a == 1
+
