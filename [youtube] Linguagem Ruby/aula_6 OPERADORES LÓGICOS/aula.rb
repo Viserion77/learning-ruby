dc2325807263c89faf9e5d2b70029ea918b7bc33
Viserion77 <jeferson.a.oficial@gmail.com>
2022-07-20 09:36:32 -0300

    aula 6

---
+++ [youtube] Linguagem Ruby/aula_6 OPERADORES LÓGICOS/aula.rb
@@ -0,0 +1,5 @@
+condition = 1==1 or 2!=2 || not 3~=|3|
+condition = 1==1 and !2==2 && 3!~|3|
+condition = 1<1 and 1<=1 or 1>1 and 1>=1
+condition = 1.equal?(1) or 1.eql?(1)
+condition = 1 <=> 1 # valor 1 < que o 2 retorna -1
